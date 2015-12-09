71513f6f2d12a8ba5bfbe52b0d1c45a0e9c7f5b1
dhanajayan <dhananjayan322@gmail.com>
2015-12-09 13:03:37 +0530

    seeker is finished

--- webpage/page3/seeker/script.js
+++ webpage/page3/seeker/script.js
@@ -2,47 +2,50 @@ $(document).ready(function() {
 	
 	$('.first').mouseover(function() {
 
-/**		$('#hidden div').each(function() {
-		//	$(this).css("z-index","-1");
-			alert($(this).attr('id'));
-		}); */
+		$('.hidden div').each(function() {
+			$(this).css("z-index","-1");
+		//	alert($(this).attr('id'));
+		});
+
+		
 		$(this).css("background-color", "white");
 		$('#first-sub').css("z-index","1");
 		
 	});
-
+	
+	
 	$('#first-sub').mouseleave(function() {
 		$(this).css("z-index","-1");
 	});
 	
 
-/**	$('.first').mouseleave(function() {
-		$('#first-sub').css("z-index","-1");
-	}); */
 	$('#first-sub').click(function() {
 		$(this).css("z-index","-1");
 	});
 
 	$('.second').mouseenter(function() {
-/**		$('#hidden div').each(function() {
+		$('.hidden div').each(function() {
 			$(this).css("z-index","-1");
-		}); */
+		//	alert($(this).attr('id'));
+		});
+		
 		$('#second-sub').css("z-index","1");
 	});
 
 	$('#second-sub').mouseleave(function() {
 		$(this).css("z-index","-1");
 	});
-/**i
-	$('.second').mouseleave(function() {
-		$('#second-sub').css("z-index","-1");
-	}); */
 	$('#second-sub').click(function() {
 		$(this).css("z-index","-1");
 	});
 
 
 	$('.third').mouseenter(function() {
+		$('.hidden div').each(function() {
+			$(this).css("z-index","-1");
+		//	alert($(this).attr('id'));
+		});
+		
 		$('#third-sub').css("z-index","1");
 	});
 
@@ -54,22 +57,20 @@ $(document).ready(function() {
 		$(this).css("z-index","-1");
 	});
 
-/**	$('.third').mouseleave(function() {
-		$('#third-sub').css("z-index","-1");
-	}); */
 	
 	$('.forth').mouseenter(function() {
+		$('.hidden div').each(function() {
+			$(this).css("z-index","-1");
+		//	alert($(this).attr('id'));
+		});
+		
 		$('#forth-sub').css("z-index","1");
-		$('#forth-sub').show();
 	});
 		
 	$('#forth-sub').mouseleave(function() {
 		$(this).css("z-index","-1");
 	});
 
-/**	$('.forth').mouseleave(function() {
-		$('#forth-sub').css("z-index","-1");
-	}); */
 	
 	$('#forth-sub').click(function() {
 		$(this).css("z-index","-1");
