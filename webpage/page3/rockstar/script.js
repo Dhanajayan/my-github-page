04e61c95b4a76721f77be0c561fc711e97734e55
dhanajayan <dhananjayan322@gmail.com>
2015-12-13 16:59:20 +0530

    Adding script to rockstar

---
+++ webpage/page3/rockstar/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    
+        $('#one').moveover(function() {
+            $(this).css('width','500').css('zIndex','3');
+        });
+
+        $('#two').moveover(function() {
+            $(this).css('width', '500').css('zIndex','3');
+        });
+  });
