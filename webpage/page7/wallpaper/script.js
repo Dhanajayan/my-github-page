4afe7ebba468db145fcc901f0e4797607f6c6c0e
dhanajayan <dhananjayan322@gmail.com>
2016-01-23 20:45:19 +0530

    new change to style in whatsnext and wallpaper

--- webpage/page7/wallpaper/script.js
+++ webpage/page7/wallpaper/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-    $('.show').mouseenter(function() {
+    $('.show').mouseover(function() {
         var $titleval = $(this).prev().text();
-        $(this).next().html('<h1>'+$titleval+'</h1><p>327 shares</p><span class="fa fa-facebook fa-2x" style="padding: 8px;"></span><span style="padding: 8px;" class="fa fa-twitter fa-2x"></span><span style="padding: 10px;"class="fa fa-google-plus fa-2x"></span> ').fadeIn();
+        $(this).next()
+        .html('<h1>'+$titleval+'</h1><p>327 shares</p><span class="fa fa-facebook fa-2x" style="padding: 8px;"></span><span style="padding: 8px;" class="fa fa-twitter fa-2x"></span><span style="padding: 10px;"class="fa fa-google-plus fa-2x"></span> ')
+        .fadeIn();
         });
     $('.show').mouseout(function() {
-        $(this).next().fadeOut();
+            $(this).next().fadeOut(); 
         });
 });
