390b2e9c5b2e4ce7000563110b5d8c0cab3828f9
dhanajayan <dhananjayan322@gmail.com>
2016-01-23 20:00:57 +0530

    wallpaper is added

---
+++ webpage/page7/wallpaper/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.show').mouseenter(function() {
+        var $titleval = $(this).prev().text();
+        $(this).next().html('<h1>'+$titleval+'</h1><p>327 shares</p><span class="fa fa-facebook fa-2x" style="padding: 8px;"></span><span style="padding: 8px;" class="fa fa-twitter fa-2x"></span><span style="padding: 10px;"class="fa fa-google-plus fa-2x"></span> ').fadeIn();
+        });
+    $('.show').mouseout(function() {
+        $(this).next().fadeOut();
+        });
+});
