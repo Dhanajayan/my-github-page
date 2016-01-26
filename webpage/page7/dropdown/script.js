e2df1bb6041c38a0ea9be5e6010ae0df31493ebf
dhanajayan <dhananjayan322@gmail.com>
2016-01-26 20:55:06 +0530

    7th page added

--- webpage/page7/dropdown/script.js
+++ webpage/page7/dropdown/script.js
@@ -5,17 +5,36 @@ $(function() {
 
     }); 
 
+  
+   
     $('.list > h4').click(function() {
         $('.inputval').val($(this).text());
-        var before = "'";
+  /**      var before = "'";
         var after = "'";
 
         var coloring = $(this).attr('value');
         var fullString = before.concat(coloring,after);
         alert(coloring);
-        $('.wrap').css('backgroundColor','yellow'); 
+        $('.wrap').css('backgroundColor',fullString); */
+              $.arrayLoader = function(selectColor) {
+                    alert(selectColor);
+                    /*
+                    var arrayOfColor = ['#2ECC71', '#3498DB', '#9B59B6', '#E74C3C'];
+                    var i=0;
+                    $(arrayOfColor).each(function() {
+                    if($(arrayOfColor)[i]==selectColor)
+                    $('body').css({background-color : $(arrayOfColor)[i]);
+                    ++i;
+                    });
 
+                    */
+                };
+
+
+        $.arrayLoader('hello');
         $('.list').fadeOut();
     });
+
+
 
 });
