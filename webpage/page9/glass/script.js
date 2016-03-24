1a83fef24dbde572fabc3b38a73e25beb348ccf7
dhanajayan <dhananjayan322@gmail.com>
2016-03-24 19:29:37 -0400

    glass is added

---
+++ webpage/page9/glass/script.js
@@ -0,0 +1,11 @@
+
+var wrap = document.getElementById('wrap');
+
+var widthwrap = window.innerWidth;
+var heightwrap = window.innerHeight;
+alert(widthwrap);
+
+//wrap.style.width = widthwrap;
+//wrap.style.height = heightwrap;
+
+alert(window.innerWidth);
